72f0efd79622d9930d418876be45a1adbe8f2dbc
Straga Severa <ought@yandex.ru>
2017-07-12 19:43:07 +0300

    Add first route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import BlogPage from './components/widgets/BlogPage'
-import MainLayout from './components/layouts/MainLayout'
+import {Router, browserHistory} from 'react-router'
+import routes from 'routes'
 
 const App = () => (
-  <MainLayout>
-    <BlogPage />
-  </MainLayout>
+  <Router history={browserHistory} routes={routes} />
 )
 
 export default App
